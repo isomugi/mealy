feabe122699e4a0754e4a463407cefcf254293e6
磯上樹 <tatsuki5820iso@gmail.com>
2019-06-14 01:09:55 +0900

    修正

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from django.shortcuts import render
 
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	latest_post_list = Post.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_post_list': latest_post_list,
+	}
+	return HttpResponse(template.render(context, request))
+
+def detail(request, post_id):
+	try:
+		Post = Post.objects.get(pk = post_id)
+	except Post.DoesNotExist:
+		raise Http404('Post does not exist.')
+	return render(request, 'polls/detail.html', { 'post': post })
