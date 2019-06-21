526eca5452d6f7f4a405295a7ff43b586727b7b0
磯上樹 <tatsuki5820iso@gmail.com>
2019-06-21 13:01:52 +0900

    ログイン認証

--- project/urls.py
+++ project/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
 	path('post/', views.post, name='post'),
 	path('mypage/', views.mypage, name='mypage'),
 	path('register/', views.register, name='register'),
-	path('login/', views.login, name='login'),
+	path('login/', views.login_view, name='login'),
+	path('logout/', views.logout_view, name='logout'),
 ]
 
 from django.conf import settings
